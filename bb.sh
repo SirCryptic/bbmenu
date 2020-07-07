84191309eb8714efc1261802f156b5773ba71bd2
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2020-07-07 23:55:55 +0100

    Create bb.sh

---
+++ bb.sh
@@ -0,0 +1,1072 @@
+#!/bin/bash
+# PhiserPrice2.5
+##############################
+##  Created By Sir Cryptic  ##
+############################## 
+## Modifiying This You Must ##
+##   Give Credits To The    ##
+##  Developer Sir Cryptic   ## 
+##############################
+##    NULL Security Team    ##
+##############################
+############################## 
+##      Developed On        ##
+##   Kali Linux 2020.2b     ##
+##############################
+clear
+#COLOUR
+red='\e[1;31m'
+yellow='\e[0;33m'
+Blue='\e[1;34m'
+Reset='\e[0;0m'
+title="Basic Bash Menu Extreme"
+echo -e '\033]2;'$title'\007'
+
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[3;39m
+-------------------------------
+(1) Sub Menu 1
+(2) Sub Menu 2
+(3) Sub Menu 3
+(4) Sub Menu 4
+(5) Sub Menu 5
+(i) Information
+-------------------------------
+       CTRL + C To Exit
+-------------------------------
+'
+option1='1'
+option2='2'
+option3='3'
+option4='4'
+option5='5'
+info='i'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"ToolSet$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$option1" ]; then                    #Option1
+clear
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[1;31m
+Sub Menu 1                
+\e[0m\e[3;39m-------------------------------
+(s1) Option
+(s2) Option
+(s3) Option
+(s4) Option
+(s5) Option
+(s6) Option
+(s7) Option
+(s8) Option
+(s9) Option
+(s10) Option
+(s11) Option
+(s12) Option
+(s13) Option
+(s14) Option
+(s15) Option
+(s16) Option
+(s17) Option
+(s18) Option
+(s19) Option
+(s20) Option
+-------------------------------
+       CTRL + C To Exit
+-------------------------------
+Press ENTER To Go To Main Menu
+'
+submenu1='s1'
+submenu2='s2'
+submenu3='s3'
+submenu4='s4'
+submenu5='s5'
+submenu6='s6'
+submenu7='s7'
+submenu8='s8'
+submenu9='s9'
+submenu10='s10'
+submenu11='s11'
+submenu12='s12'
+submenu13='s13'
+submenu14='s14'
+submenu15='s15'
+submenu16='s16'
+submenu17='s17'
+submenu18='s18'
+submenu19='s19'
+submenu20='s20'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"Sub Menu 1$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$submenu1" ]; then                    #submenu-Option-1
+clear
+echo "Sub Option 1"
+
+read
+
+elif [ "$x" == "$submenu2" ]; then                    #submenu-Option-2
+clear
+echo "Sub Option 2"
+
+read
+
+elif [ "$x" == "$submenu3" ]; then                    #submenu-Option-3
+clear
+echo "Sub Option 3"
+
+read
+
+
+elif [ "$x" == "$submenu4" ]; then                    #submenu-Option-4
+clear
+echo "Sub Option 4"
+
+read
+
+elif [ "$x" == "$submenu5" ]; then                    #submenu-Option-5
+clear
+echo "Sub Option 5"
+
+read
+
+elif [ "$x" == "$submenu6" ]; then                    #submenu-Option-6
+clear
+echo "Sub Option 6"
+
+read
+
+elif [ "$x" == "$submenu7" ]; then                    #submenu-Option-7
+clear
+echo "Sub Option 7"
+
+read
+
+elif [ "$x" == "$submenu8" ]; then                    #submenu-Option-8
+clear
+echo "Sub Option 8"
+
+read
+
+elif [ "$x" == "$submenu9" ]; then                    #submenu-Option-9
+clear
+echo "Sub Option 9"
+
+read
+
+elif [ "$x" == "$submenu10" ]; then                    #submenu-Option-10
+clear
+echo "Sub Option 10"
+
+read
+
+elif [ "$x" == "$submenu11" ]; then                    #submenu-Option-11
+clear
+echo "Sub Option 11"
+
+read
+
+elif [ "$x" == "$submenu12" ]; then                    #submenu-Option-12
+clear
+echo "Sub Option 12"
+
+read
+
+elif [ "$x" == "$submenu13" ]; then                    #submenu-Option-13
+clear
+clear
+echo "Sub Option 13"
+
+read
+
+elif [ "$x" == "$submenu14" ]; then                    #submenu-Option-14
+clear
+echo "Sub Option 14"
+
+read
+
+elif [ "$x" == "$submenu15" ]; then                    #submenu-Option-15
+clear
+echo "Sub Option 15"
+
+read
+
+elif [ "$x" == "$submenu16" ]; then                    #submenu-Option-16
+clear
+echo "Sub Option 16"
+
+read
+
+
+elif [ "$x" == "$submenu17" ]; then                    #submenu-Option-17
+clear
+echo "Sub Option 17"
+
+read
+
+elif [ "$x" == "$submenu18" ]; then                    #submenu-Option-18
+clear
+echo "Sub Option 18"
+
+read
+
+elif [ "$x" == "$submenu19" ]; then                    #submenu-Option-19
+clear
+echo "Sub Option 19"
+
+read
+
+
+elif [ "$x" == "$submenu19" ]; then                    #submenu-Option-19
+clear
+echo "Sub Option 20"
+
+read
+
+else 
+
+n
+
+
+fi
+
+elif [ "$x" == "$option2" ]; then                          #Option2
+clear
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[1;31m
+Sub Menu 2                
+\e[0m\e[3;39m-------------------------------
+(a1) Option
+(a2) Option
+(a3) Option
+(a4) Option
+(a5) Option
+(a6) Option
+(a7) Option
+(a8) Option
+(a9) Option
+(a10) Option
+(a11) Option
+(a12) Option
+(a13) Option
+(a14) Option
+(a15) Option
+(a16) Option
+(a17) Option
+(a18) Option
+(a19) Option
+(a20) Option
+------------------------------
+CTRL + C To Exit
+------------------------------
+Press ENTER To Go To Main Menu
+'
+submenu11='a1'
+submenu12='a2'
+submenu13='a3'
+submenu14='a4'
+submenu15='a5'
+submenu16='a6'
+submenu17='a7'
+submenu18='a8'
+submenu19='a9'
+submenu110='a10'
+submenu111='a11'
+submenu112='a12'
+submenu113='a13'
+submenu114='a14'
+submenu115='a15'
+submenu116='a16'
+submenu117='a17'
+submenu118='a18'
+submenu119='a19'
+submenu120='a20'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"Sub Menu 2$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$submenu11" ]; then                    #submenu1-Option-1
+clear
+echo "Sub Option 1"
+
+read
+
+elif [ "$x" == "$submenu12" ]; then                    #submenu1-Option-2
+clear
+echo "Sub Option 2"
+
+read
+
+elif [ "$x" == "$submenu13" ]; then                    #submenu1-Option-3
+clear
+echo "Sub Option 3"
+
+read
+
+
+elif [ "$x" == "$submenu14" ]; then                    #submenu1-Option-4
+clear
+echo "Sub Option 4"
+
+read
+
+elif [ "$x" == "$submenu15" ]; then                    #submenu1-Option-5
+clear
+echo "Sub Option 5"
+
+read
+
+elif [ "$x" == "$submenu16" ]; then                    #submenu1-Option-6
+clear
+echo "Sub Option 6"
+
+read
+
+elif [ "$x" == "$submenu17" ]; then                    #submenu1-Option-7
+clear
+echo "Sub Option 7"
+
+read
+
+elif [ "$x" == "$submenu18" ]; then                    #submenu1-Option-8
+clear
+echo "Sub Option 8"
+
+read
+
+elif [ "$x" == "$submenu19" ]; then                    #submenu1-Option-9
+clear
+echo "Sub Option 9"
+
+read
+
+elif [ "$x" == "$submenu110" ]; then                    #submenu1-Option-10
+clear
+echo "Sub Option 10"
+
+read
+
+elif [ "$x" == "$submenu111" ]; then                    #submenu1-Option-11
+clear
+echo "Sub Option 11"
+
+read
+
+elif [ "$x" == "$submenu112" ]; then                    #submenu1-Option-12
+clear
+echo "Sub Option 12"
+
+read
+
+elif [ "$x" == "$submenu113" ]; then                    #submenu1-Option-13
+clear
+clear
+echo "Sub Option 13"
+
+read
+
+elif [ "$x" == "$submenu114" ]; then                    #submenu1-Option-14
+clear
+echo "Sub Option 14"
+
+read
+
+elif [ "$x" == "$submenu115" ]; then                    #submenu1-Option-15
+clear
+echo "Sub Option 15"
+
+read
+
+elif [ "$x" == "$submenu116" ]; then                    #submenu1-Option-16
+clear
+echo "Sub Option 16"
+
+read
+
+
+elif [ "$x" == "$submenu117" ]; then                    #submenu1-Option-17
+clear
+echo "Sub Option 17"
+
+read
+
+elif [ "$x" == "$submenu118" ]; then                    #submenu1-Option-18
+clear
+echo "Sub Option 18"
+
+read
+
+elif [ "$x" == "$submenu119" ]; then                    #submenu1-Option-19
+clear
+echo "Sub Option 19"
+
+read
+
+
+elif [ "$x" == "$submenu119" ]; then                    #submenu1-Option-19
+clear
+echo "Sub Option 20"
+
+read
+
+else 
+
+n
+
+
+fi
+
+elif [ "$x" == "$option3" ]; then                          #Option3
+clear
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[1;31m
+Sub Menu 3                
+\e[0m\e[3;39m-------------------------------
+(w1) Option
+(w2) Option
+(w3) Option
+(w4) Option
+(w5) Option
+(w6) Option
+(w7) Option
+(w8) Option
+(w9) Option
+(w10) Option
+(w11) Option
+(w12) Option
+(w13) Option
+(w14) Option
+(w15) Option
+(w16) Option
+(w17) Option
+(w18) Option
+(w19) Option
+(s20) Option
+-------------------------------
+       CTRL + C To Exit
+-------------------------------
+Press ENTER To Go To Main Menu
+'
+submenu21='w1'
+submenu22='w2'
+submenu23='w3'
+submenu24='w4'
+submenu25='w5'
+submenu26='w6'
+submenu27='w7'
+submenu28='w8'
+submenu29='w9'
+submenu210='w10'
+submenu211='w11'
+submenu212='w12'
+submenu213='w13'
+submenu214='w14'
+submenu215='w15'
+submenu216='w16'
+submenu217='w17'
+submenu218='w18'
+submenu219='w19'
+submenu220='w20'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"Sub Menu 3$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$submenu21" ]; then                    #submenu2-Option-1
+clear
+echo "Sub Option 1"
+
+read
+
+elif [ "$x" == "$submenu22" ]; then                    #submenu2-Option-2
+clear
+echo "Sub Option 2"
+
+read
+
+elif [ "$x" == "$submenu23" ]; then                    #submenu2-Option-3
+clear
+echo "Sub Option 3"
+
+read
+
+
+elif [ "$x" == "$submenu24" ]; then                    #submenu2-Option-4
+clear
+echo "Sub Option 4"
+
+read
+
+elif [ "$x" == "$submenu25" ]; then                    #submenu2-Option-5
+clear
+echo "Sub Option 5"
+
+read
+
+elif [ "$x" == "$submenu26" ]; then                    #submenu2-Option-6
+clear
+echo "Sub Option 6"
+
+read
+
+elif [ "$x" == "$submenu27" ]; then                    #submenu2-Option-7
+clear
+echo "Sub Option 7"
+
+read
+
+elif [ "$x" == "$submenu28" ]; then                    #submenu2-Option-8
+clear
+echo "Sub Option 8"
+
+read
+
+elif [ "$x" == "$submenu29" ]; then                    #submenu2-Option-9
+clear
+echo "Sub Option 9"
+
+read
+
+elif [ "$x" == "$submenu210" ]; then                    #submenu2-Option-10
+clear
+echo "Sub Option 10"
+
+read
+
+elif [ "$x" == "$submenu211" ]; then                    #submenu2-Option-11
+clear
+echo "Sub Option 11"
+
+read
+
+elif [ "$x" == "$submenu212" ]; then                    #submenu2-Option-12
+clear
+echo "Sub Option 12"
+
+read
+
+elif [ "$x" == "$submenu213" ]; then                    #submenu2-Option-13
+clear
+clear
+echo "Sub Option 13"
+
+read
+
+elif [ "$x" == "$submenu214" ]; then                    #submenu2-Option-14
+clear
+echo "Sub Option 14"
+
+read
+
+elif [ "$x" == "$submenu215" ]; then                    #submenu2-Option-15
+clear
+echo "Sub Option 15"
+
+read
+
+elif [ "$x" == "$submenu216" ]; then                    #submenu2-Option-16
+clear
+echo "Sub Option 16"
+
+read
+
+
+elif [ "$x" == "$submenu217" ]; then                    #submenu2-Option-17
+clear
+echo "Sub Option 17"
+
+read
+
+elif [ "$x" == "$submenu218" ]; then                    #submenu2-Option-18
+clear
+echo "Sub Option 18"
+
+read
+
+elif [ "$x" == "$submenu219" ]; then                    #submenu2-Option-19
+clear
+echo "Sub Option 19"
+
+read
+
+
+elif [ "$x" == "$submenu219" ]; then                    #submenu2-Option-19
+clear
+echo "Sub Option 20"
+
+read
+
+else 
+
+n
+
+
+fi
+elif [ "$x" == "$option4" ]; then                          #Option3
+clear
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[1;31m
+Sub Menu 4                
+\e[0m\e[3;39m-------------------------------
+(d1) Option
+(d2) Option
+(d3) Option
+(d4) Option
+(d5) Option
+(d6) Option
+(d7) Option
+(d8) Option
+(d9) Option
+(d10) Option
+(d11) Option
+(d12) Option
+(d13) Option
+(d14) Option
+(d15) Option
+(d16) Option
+(d17) Option
+(d18) Option
+(d19) Option
+(d20) Option
+-------------------------------
+       CTRL + C To Exit
+-------------------------------
+Press ENTER To Go To Main Menu
+'
+submenu31='d1'
+submenu32='d2'
+submenu33='d3'
+submenu34='d4'
+submenu35='d5'
+submenu36='d6'
+submenu37='d7'
+submenu38='d8'
+submenu39='d9'
+submenu310='d10'
+submenu311='d11'
+submenu312='d12'
+submenu313='d13'
+submenu314='d14'
+submenu315='d15'
+submenu316='d16'
+submenu317='d17'
+submenu318='d18'
+submenu319='d19'
+submenu320='d20'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"Sub Menu 4$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$submenu31" ]; then                    #submenu3-Option-1
+clear
+echo "Sub Option 1"
+
+read
+
+elif [ "$x" == "$submenu32" ]; then                    #submenu3-Option-2
+clear
+echo "Sub Option 2"
+
+read
+
+elif [ "$x" == "$submenu33" ]; then                    #submenu3-Option-3
+clear
+echo "Sub Option 3"
+
+read
+
+
+elif [ "$x" == "$submenu34" ]; then                    #submenu3-Option-4
+clear
+echo "Sub Option 4"
+
+read
+
+elif [ "$x" == "$submenu35" ]; then                    #submenu3-Option-5
+clear
+echo "Sub Option 5"
+
+read
+
+elif [ "$x" == "$submenu36" ]; then                    #submenu3-Option-6
+clear
+echo "Sub Option 6"
+
+read
+
+elif [ "$x" == "$submenu37" ]; then                    #submenu3-Option-7
+clear
+echo "Sub Option 7"
+
+read
+
+elif [ "$x" == "$submenu38" ]; then                    #submenu3-Option-8
+clear
+echo "Sub Option 8"
+
+read
+
+elif [ "$x" == "$submenu39" ]; then                    #submenu3-Option-9
+clear
+echo "Sub Option 9"
+
+read
+
+elif [ "$x" == "$submenu310" ]; then                    #submenu3-Option-10
+clear
+echo "Sub Option 10"
+
+read
+
+elif [ "$x" == "$submenu311" ]; then                    #submenu3-Option-11
+clear
+echo "Sub Option 11"
+
+read
+
+elif [ "$x" == "$submenu312" ]; then                    #submenu3-Option-12
+clear
+echo "Sub Option 12"
+
+read
+
+elif [ "$x" == "$submenu313" ]; then                    #submenu3-Option-13
+clear
+clear
+echo "Sub Option 13"
+
+read
+
+elif [ "$x" == "$submenu314" ]; then                    #submenu3-Option-14
+clear
+echo "Sub Option 14"
+
+read
+
+elif [ "$x" == "$submenu315" ]; then                    #submenu3-Option-15
+clear
+echo "Sub Option 15"
+
+read
+
+elif [ "$x" == "$submenu316" ]; then                    #submenu3-Option-16
+clear
+echo "Sub Option 16"
+
+read
+
+
+elif [ "$x" == "$submenu317" ]; then                    #submenu3-Option-17
+clear
+echo "Sub Option 17"
+
+read
+
+elif [ "$x" == "$submenu318" ]; then                    #submenu3-Option-18
+clear
+echo "Sub Option 18"
+
+read
+
+elif [ "$x" == "$submenu319" ]; then                    #submenu3-Option-19
+clear
+echo "Sub Option 19"
+
+read
+
+
+elif [ "$x" == "$submenu319" ]; then                    #submenu3-Option-19
+clear
+echo "Sub Option 20"
+
+read
+
+else 
+
+n
+
+
+fi
+elif [ "$x" == "$option5" ]; then                          #Option3
+clear
+echo -e '\e[1;33m
+///,        ////
+\  /,      /  >.
+ \  /,   _/  /.
+  \_  /_/   /.
+   \__/_   <    BBMenu Extreme
+   /<<< \_\_  Serve & Demolish
+  /,)^>>_._ \ Version 1.0.0 [BETA] 
+  (/   \\ /\\\
+       // //```
+======((`((====\e[1;34m
+'
+echo -e '\e[3;34m                 
+\e[0m\e[1;31m
+Sub Menu 5                
+\e[0m\e[3;39m-------------------------------
+(e1) Option
+(e2) Option
+(e3) Option
+(e4) Option
+(e5) Option
+(e6) Option
+(e7) Option
+(e8) Option
+(e9) Option
+(e10) Option
+(e11) Option
+(e12) Option
+(e13) Option
+(e14) Option
+(e15) Option
+(e16) Option
+(e17) Option
+(e18) Option
+(e19) Option
+(e20) Option
+-------------------------------
+       CTRL + C To Exit
+-------------------------------
+Press ENTER To Go To Main Menu
+'
+submenu41='e1'
+submenu42='e2'
+submenu43='e3'
+submenu44='e4'
+submenu45='e5'
+submenu46='e6'
+submenu47='e7'
+submenu48='e8'
+submenu49='e9'
+submenu410='e10'
+submenu411='e11'
+submenu412='e12'
+submenu413='e13'
+submenu414='e14'
+submenu415='e15'
+submenu416='e16'
+submenu417='e17'
+submenu418='e18'
+submenu419='e19'
+submenu420='e20'
+
+echo -e $Blue" ┌─["$red"BBMenu Extreme$Blue]──[$red~$Blue]─["$yellow"Sub Menu 5$Blue]:"
+echo -e $Blue" └─────► " ;read -p " Option: " x
+
+if [ "$x" == "$submenu41" ]; then                    #submenu4-Option-1
+clear
+echo "Sub Option 1"
+
+read
+
+elif [ "$x" == "$submenu42" ]; then                    #submenu4-Option-2
+clear
+echo "Sub Option 2"
+
+read
+
+elif [ "$x" == "$submenu43" ]; then                    #submenu4-Option-3
+clear
+echo "Sub Option 3"
+
+read
+
+
+elif [ "$x" == "$submenu44" ]; then                    #submenu4-Option-4
+clear
+echo "Sub Option 4"
+
+read
+
+elif [ "$x" == "$submenu45" ]; then                    #submenu4-Option-5
+clear
+echo "Sub Option 5"
+
+read
+
+elif [ "$x" == "$submenu46" ]; then                    #submenu4-Option-6
+clear
+echo "Sub Option 6"
+
+read
+
+elif [ "$x" == "$submenu47" ]; then                    #submenu4-Option-7
+clear
+echo "Sub Option 7"
+
+read
+
+elif [ "$x" == "$submenu48" ]; then                    #submenu4-Option-8
+clear
+echo "Sub Option 8"
+
+read
+
+elif [ "$x" == "$submenu49" ]; then                    #submenu4-Option-9
+clear
+echo "Sub Option 9"
+
+read
+
+elif [ "$x" == "$submenu410" ]; then                    #submenu4-Option-10
+clear
+echo "Sub Option 10"
+
+read
+
+elif [ "$x" == "$submenu411" ]; then                    #submenu4-Option-11
+clear
+echo "Sub Option 11"
+
+read
+
+elif [ "$x" == "$submenu412" ]; then                    #submenu4-Option-12
+clear
+echo "Sub Option 12"
+
+read
+
+elif [ "$x" == "$submenu413" ]; then                    #submenu4-Option-13
+clear
+clear
+echo "Sub Option 13"
+
+read
+
+elif [ "$x" == "$submenu414" ]; then                    #submenu4-Option-14
+clear
+echo "Sub Option 14"
+
+read
+
+elif [ "$x" == "$submenu415" ]; then                    #submenu4-Option-15
+clear
+echo "Sub Option 15"
+
+read
+
+elif [ "$x" == "$submenu416" ]; then                    #submenu4-Option-16
+clear
+echo "Sub Option 16"
+
+read
+
+
+elif [ "$x" == "$submenu417" ]; then                    #submenu4-Option-17
+clear
+echo "Sub Option 17"
+
+read
+
+elif [ "$x" == "$submenu418" ]; then                    #submenu4-Option-18
+clear
+echo "Sub Option 18"
+
+read
+
+elif [ "$x" == "$submenu419" ]; then                    #submenu4-Option-19
+clear
+echo "Sub Option 19"
+
+read
+
+
+elif [ "$x" == "$submenu419" ]; then                    #submenu4-Option-19
+clear
+echo "Sub Option 20"
+
+read
+
+else 
+
+n
+
+
+fi
+elif [ "$x" == "$info" ]; then                 #INFO                    
+
+clear
+
+echo -e '\e[1;33m
+\e[0m
+
+ Created By Sir Cryptic ~ SCNS
+
+ \e[1;31m
+ no-reply@nullsec.online
+ https://nullsec.online
+
+
+                          Press ENTER To Go Back To The Main Menu
+'
+read
+
+else 
+
+n
+
+
+fi
+
+done
